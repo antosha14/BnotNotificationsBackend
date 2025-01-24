168499980c49b65b11e343c2b8c2d925fc7caa9e
unknown <anton.kozel.97@mail.ru>
2025-01-25 02:31:22 +0300

    feat: change notification text

--- netlify/functions/inform.js
+++ netlify/functions/inform.js
@@ -1,39 +1,40 @@
 exports.handler = async (req) => {
-  try {    
-    const token = String(req.queryStringParameters.token)
-    const regnum = String(req.queryStringParameters.regnum)
-    const link = String(req.queryStringParameters.link)
-    const queueName = String(req.queryStringParameters.queueName)
-    const notifyPosition = Number(req.queryStringParameters.notifyPosition)
+  try {
+    const token = String(req.queryStringParameters.token);
+    const regnum = String(req.queryStringParameters.regnum);
+    const link = String(req.queryStringParameters.link);
+    const queueName = String(req.queryStringParameters.queueName);
+    const notifyPosition = Number(req.queryStringParameters.notifyPosition);
 
     const response = await fetch(link);
     const data = await response.json();
 
-    const newPosition = data[queueName].findIndex((trip) => {
+    const newPosition =
+      data[queueName].findIndex((trip) => {
         return trip.regnum === regnum;
-    }) + 1;
-    
-    if(newPosition <= notifyPosition){
-      await fetch('https://exp.host/--/api/v2/push/send', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      }) + 1;
+
+    if (newPosition <= notifyPosition) {
+      await fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           to: token,
-          sound: 'default',
-          priority: 'high',
+          sound: "default",
+          priority: "high",
           body: `It is your turn to pass border control. Your current position is ${newPosition}`,
         }),
-    });
+      });
     }
 
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `New position: ${newPosition}` }),
     };
-  } catch(err){
+  } catch (err) {
     return {
       statusCode: 500,
-      body: JSON.stringify(err)
+      body: JSON.stringify(err),
     };
   }
 };
